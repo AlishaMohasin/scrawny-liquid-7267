9a5efeac35c4150691f2ded0adb50d3e1c8f3d83
Alisha <alishamohasin85@gmail.com>
2022-08-24 00:25:50 +0530

    home page

---
+++ mondayapp/src/Components/Topsec.jsx
@@ -0,0 +1,55 @@
+import { Box, Heading,Image} from '@chakra-ui/react'
+import React from 'react'
+import style from "./Topsec.module.css"
+
+const Topsec = () => {
+    return (
+        <>
+            <Box className={style.first}>
+
+                <h1 className={style.h1}>A platform built for a <br />
+                
+                new way of working
+                </h1>
+                <h3 className={style.h3}>What would you like to manage with monday.com Work OS?</h3>
+              
+               
+
+            </Box>
+             <Image className={style.img} src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/image.png" /> 
+
+        </>
+  
+  )
+}
+
+export default Topsec
+// max-width: 1440px;
+//     margin: auto;
+//     height: 600px;
+//     display: -webkit-box;
+//     display: -webkit-flex;
+//     display: -ms-flexbox;
+//     display: flex;
+//     -webkit-flex-direction: column;
+//     -ms-flex-direction: column;
+//     flex-direction: column;
+//     position: relative;
+//     -webkit-align-items: center;
+//     -webkit-box-align: center;
+//     -ms-flex-align: center;
+//     align-items: center;
+//     -webkit-box-pack: center;
+//     -webkit-justify-content: center;
+//     -ms-flex-pack: center;
+//     justify-content: center;
+
+
+
+
+
+
+// line-height: 1.15;
+//     -ms-text-size-adjust: 100%;
+//     -webkit-text-size-adjust: 100%;
+// }
---
+++ mondayapp/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topsec from '../Components/Topsec'
+
+const Home = () => {
+  return (
+      <div>
+          <Topsec/>
+    </div>
+  )
+}
+
+export default Home
